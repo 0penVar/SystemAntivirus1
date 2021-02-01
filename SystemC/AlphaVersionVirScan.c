6d003f77e9e138799ff90172fe5a8a4645fdeb59
0penVar <71385626+0penVar@users.noreply.github.com>
2021-01-31 23:58:34 -0500

    Update AlphaVersionVirScan.c

--- SystemC/AlphaVersionVirScan.c
+++ SystemC/AlphaVersionVirScan.c
@@ -4,22 +4,293 @@
 
 int main(int argc, char *argv[])
 {
-  float varVir = 0;
-  
-  printf("\n");
-  printf("Entrez le numero du fichier a analyser\n");
-  scanf("%f", &varVir);
-  
-  if(varVir >= 1000)
-  {
-    printf("Alerte ! UN LOGICIEL MALVEILLANT A ETE DETECTE !\n");
-  }
-  else if(varVir >= 700)
-  {
-    printf("Alerte ! Logiciel potentiellement malveillant detecte !\n");
-  }
-  else
-  {
-    printf("Aucun logiciel malveillant detecte");
-  }
+    float var = 0, var2 = 0, var3 = 0; // 16,16,16
+    float exe = 0, exe2 = 3;
+    exe = pow(exe2, 2); // 4a
+
+    printf("\n"); // 1
+    printf("entrez une signature logiciel : "); // 21
+    scanf("%f", &var2); // 21
+    printf("entrez un comportement :"); // 21
+    scanf("%f", &var3); // 21
+
+    var = var2 + var3; // 8
+    var++;
+
+    printf("\n"); // 1
+    printf("%f + %f = %f\n", var3, var2, var); // 14, 1a
+
+    if(var < 100 && var >= 85)
+    {
+        printf("\n");
+        printf("Alerte ! UN CHEVAL DE TROIE A ETE DETECTE !");
+    }
+    else if(var == 100)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 101)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 102)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 103)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 104)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 105)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 106)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 107)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 108)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 109)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 110)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 111)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 112)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 113)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 114)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 115)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 116)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 117)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 118)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 119)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 120)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 121)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 122)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 123)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 124)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 125)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 126)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 127)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 128)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 129)
+    {
+        printf("\n");
+        printf("Alerte ! UN VER INFORMATIQUE A ETE DETECTE !");
+    }
+    else if(var == 130)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 131)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 132)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 133)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 134)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 135)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 136)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 137)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 138)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 139)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 140)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 141)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 142)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 143)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 144)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 145)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 146)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 147)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 148)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var == 149)
+    {
+        printf("\n");
+        printf("Alerte ! UNE PORTE DEROBE A ETE DETECTE DANS L'APPLICATION !\n");
+    }
+    else if(var >= 150)
+    {
+        printf("\n");
+        printf("Alerte ! UN VIRUS INFORMATIQUE A ETE DETECTE !\n");
+    }
+    else if(var >= 60)
+    {
+        printf("Logiciel potentiellement malveillant detecte");
+    }
+    else if(var <= 0)
+    {
+        printf("Aucun Logiciel malveillant detecte");
+    }
+    else
+    {
+        printf("erreur");
+    }
+    return 0; // 15
 }
